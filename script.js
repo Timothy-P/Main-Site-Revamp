85dadd1f478f6d6af29a58d14f9bfd1e7aaec907
Timothy <128176649+Timothy-P@users.noreply.github.com>
2025-04-28 07:42:12 -0500

    Uploading the latest changes.
    I've been doing a lot of work and thought to upload what I've done.

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 // Perminent stuff.
-function dragElement(elmnt) {
+function dragElementSnap(elmnt) {
     let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
     let elmnX = 0, elmnY = 0, xdiff = 0, ydiff = 0;
     elmnt.onmousedown = dragMouseDown;
@@ -70,6 +70,60 @@ function dragElement(elmnt) {
         elmnt.style.left = "0px";
     }
 }
+;
+function dragElement(elmnt) {
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    let elmnX = 0, elmnY = 0, xdiff = 0, ydiff = 0;
+    elmnt.onmousedown = dragMouseDown;
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        if (elmnt.dataset.left === undefined && elmnt.dataset.top === undefined) {
+            elmnt.dataset.left = "0";
+            elmnt.dataset.top = "0";
+        }
+        if (elmnt.style.left.length === 0 && elmnt.style.top.length === 0) {
+            elmnX = elmnt.clientLeft;
+            elmnY = elmnt.clientTop;
+        }
+        else {
+            elmnX = parseInt(elmnt.style.left.slice(0, -2));
+            elmnY = parseInt(elmnt.style.top.slice(0, -2));
+        }
+        ;
+        // Do difference???
+        xdiff = (e.clientX - elmnX);
+        ydiff = (e.clientY - elmnY);
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        let elementX = e.clientX - xdiff;
+        let elementY = e.clientY - ydiff;
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        //elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        //elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        elmnt.style.top = (e.clientY - ydiff) + "px";
+        elmnt.style.left = (e.clientX - xdiff) + "px";
+    }
+    function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+;
 function create(content, elementType, target, styles, id) {
     if (content === void 0) {
         content = "";
@@ -91,22 +145,27 @@ function create(content, elementType, target, styles, id) {
     target === null || target === void 0 ? void 0 : target.append(newDiv);
     return newDiv;
 }
-function menu(bodyContent, id, target) {
+;
+function menu(bodyContent, id, target, e) {
     // Base
-    let body = create("", "div", target, "", id);
-    let header = create("", "header", body, "", id + "-header");
-    let footer = create("", "footer", body, "", id + "-footer");
+    let body = create("", "div", target, `min-height: 50px; top: ${e.clientY}px; left: ${e.clientX}px; position: absolute; background: #ffffffff; min-width: 100px`, id);
+    let header = create("", "header", body, "min-height: 25px; border-color: 818182ff;", id + "-header");
+    let footer = create("", "footer", body, "min-height: 25px; border-color: 818182ff;", id + "-footer");
     let Close = create("", "div", header, "justify-content:end;", id + "close");
+    let closebutton = create("", "img", Close, "", "closebutton");
+    //closebutton.attributes.item. = "./Icons/Cross.ico"
+    dragElement(header);
     Close.onclick = function () {
         // 
         body.remove();
     };
+    return true;
 }
+;
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 window.addEventListener("DOMContentLoaded", function () {
-    let i = 0;
     for (let i = 0; i < this.document.getElementById("mainGrid").childElementCount; i++) {
-        dragElement(this.document.getElementById("mainGrid").children[i]);
+        dragElementSnap(this.document.getElementById("mainGrid").children[i]);
     }
     //dragElement(this.document.getElementById("temp")!);
     //dragElement(this.document.getElementById("Help")!);
@@ -119,7 +178,7 @@ function errorhandle(e) {
 this.document.getElementById("temp").addEventListener("dblclick", function (e) {
     let target = e.target, parent = target.parentElement.parentElement;
     alert("Temp double clicked");
-    menu("", "tempMenu", document.getElementById("main"));
+    menu("", "tempMenu", document.getElementById("main"), e) ? console.log("Menu created") : console.log("Menu not created");
 });
 let jsondata;
 // File system
