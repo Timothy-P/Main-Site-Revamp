fafad37dce4d289cd9fbe7e15eb803c77664fa8f
Timothy <128176649+Timothy-P@users.noreply.github.com>
2025-04-29 06:26:30 -0500

    Updating files
    I'm just updating the files, as I like to work on them in my own time.

--- script.js
+++ script.js
@@ -13,8 +13,6 @@ function dragElementSnap(elmnt) {
     let elmnX = 0, elmnY = 0, xdiff = 0, ydiff = 0;
     elmnt.onmousedown = dragMouseDown;
     function dragMouseDown(e) {
-        e = e || window.event;
-        e.preventDefault();
         // get the mouse cursor position at startup:
         pos3 = e.clientX;
         pos4 = e.clientY;
@@ -39,8 +37,6 @@ function dragElementSnap(elmnt) {
         document.onmousemove = elementDrag;
     }
     function elementDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
         // calculate the new cursor position:
         let elementX = e.clientX - xdiff;
         let elementY = e.clientY - ydiff;
@@ -76,8 +72,6 @@ function dragElement(elmnt) {
     let elmnX = 0, elmnY = 0, xdiff = 0, ydiff = 0;
     elmnt.onmousedown = dragMouseDown;
     function dragMouseDown(e) {
-        e = e || window.event;
-        e.preventDefault();
         // get the mouse cursor position at startup:
         pos3 = e.clientX;
         pos4 = e.clientY;
@@ -102,8 +96,6 @@ function dragElement(elmnt) {
         document.onmousemove = elementDrag;
     }
     function elementDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
         // calculate the new cursor position:
         let elementX = e.clientX - xdiff;
         let elementY = e.clientY - ydiff;
@@ -116,6 +108,56 @@ function dragElement(elmnt) {
         //elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
         elmnt.style.top = (e.clientY - ydiff) + "px";
         elmnt.style.left = (e.clientX - xdiff) + "px";
+    }
+    function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+;
+function dragMenu(elmnt) {
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    let elmnX = 0, elmnY = 0, xdiff = 0, ydiff = 0;
+    elmnt.onmousedown = dragMouseDown;
+    let parent = elmnt.parentElement;
+    function dragMouseDown(e) {
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        if (parent.dataset.left === undefined && parent.dataset.top === undefined) {
+            parent.dataset.left = "0";
+            parent.dataset.top = "0";
+        }
+        if (parent.style.left.length === 0 && parent.style.top.length === 0) {
+            elmnX = parent.clientLeft;
+            elmnY = parent.clientTop;
+        }
+        else {
+            elmnX = parseInt(parent.style.left.slice(0, -2));
+            elmnY = parseInt(parent.style.top.slice(0, -2));
+        }
+        ;
+        // Do difference???
+        xdiff = (e.clientX - elmnX);
+        ydiff = (e.clientY - elmnY);
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+    function elementDrag(e) {
+        // calculate the new cursor position:
+        let elementX = e.clientX - xdiff;
+        let elementY = e.clientY - ydiff;
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = "0px";
+        elmnt.style.left = "0px";
+        parent.style.top = (e.clientY - ydiff) + "px";
+        parent.style.left = (e.clientX - xdiff) + "px";
     }
     function closeDragElement() {
         // stop moving when mouse button is released:
@@ -148,17 +190,16 @@ function create(content, elementType, target, styles, id) {
 ;
 function menu(bodyContent, id, target, e) {
     // Base
-    let body = create("", "div", target, `min-height: 150px; top: ${e.clientY}px; left: ${e.clientX}px; position: absolute; background: #ffffffff; min-width: 150px`, id);
-    let header = create("", "header", body, "min-height: 25px; border-color: #818182ff; display: flex; background-color: aqua; justify-content:end;", id + "-header");
-    let footer = create("", "footer", body, "min-height: 25px; border-color: #818182ff;", id + "-footer");
-    let Close = create("", "div", header, "background-color: black; width: 25px; height: 25px;", id + "close");
-    let closebutton = create("", "img", Close, "", "closebutton");
-    //closebutton.attributes.item. = "./Icons/Cross.ico"
-    dragElement(header);
-    closebutton.setAttribute("src","./Icons/Cross.ico");
+    let wrapper = create("", "div", target, `height: 150px; top: ${e.clientY}px; left: ${e.clientX}px; position: absolute; background: #ffffffff; width: 300px`, id);
+    let header = create("", "header", wrapper, "height: 25px;border-bottom: #818182ff solid 3px;display: flex;background-color: #c2c2c2ff;justify-content: end;top: 0px;left: 0px;", id + "-header");
+    let body = create("", "div", wrapper, "height: 94px;width: -webkit-fill-available;", id + "-body");
+    let footer = create("", "div", wrapper, "width: -webkit-fill-available;min-height: 25px;background: #c2c2c2ff;border-top: #818182ff solid 3px;", id + "-footer");
+    let Close = create("", "div", header, "justify-content:end;height: -webkit-fill-available;", id + "close");
+    let closebutton = create("", "img", Close, "height: -webkit-fill-available; padding: 5px;", "closebutton");
+    dragMenu(header);
+    closebutton.setAttribute("src", "./Icons/WindowCross.png");
     Close.onclick = function () {
-        // 
-        body.remove();
+        wrapper.remove();
     };
     return true;
 }
@@ -176,9 +217,9 @@ function errorhandle(e) {
     // 
     create(`${e}`, "div", document.getElementById("main"), `background:`, "errormenu");
 }
+;
 this.document.getElementById("temp").addEventListener("dblclick", function (e) {
     let target = e.target, parent = target.parentElement.parentElement;
-    alert("Temp double clicked");
     menu("", "tempMenu", document.getElementById("main"), e) ? console.log("Menu created") : console.log("Menu not created");
 });
 let jsondata;
